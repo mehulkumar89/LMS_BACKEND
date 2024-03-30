34a1cb83e7827ff664610a189adf96502ae66786
mehulkumar89 <115225273+mehulkumar89@users.noreply.github.com>
2024-03-30 13:26:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ export const razorpay=new Razorpay({
     key_id:process.env.RAZOR_PAY_API,
     key_secret:process.env.RAZOR_PAY_SECRET
 })
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
 app.listen(process.env.PORT || 3000,async()=>{
     await connectiondb()
     console.log(`server is running on ${port}`);
